c3f60c604b88efe902e81fac04d0c563d600dd9c
paros <paroscrangsha@gmail.com>
2025-05-14 01:52:39 +0600

    login update

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,30 @@
 import React, { useEffect, useRef, useState } from 'react';
 import Container from './Container';
 import { Link } from 'react-router-dom';
+import { FaUser } from "react-icons/fa";
 
 const Header = () => {
   const [isUpdateOpen, setIsUpdateOpen] = useState(false);
   const [isReportOpen, setIsReportOpen] = useState(false);
   let reportref = useRef()
   let updateref = useRef()
+
+  let [showupdate, setshowupdate] = useState(false)
+  let [user, setUser] = useState('')
+  let [password, setPassword] = useState('')
+
+  let [showuser, setShowUser] = useState(false)
+
+  let showUser= ()=>{
+    setShowUser(!showuser)
+  }
+  let handleLogin = ()=>{
+    if (user == 'Paros' && password == 421) {
+      setshowupdate(!showupdate)
+    }else{
+      setshowupdate(false)
+    }
+  }
 
   useEffect(()=>{
     document.addEventListener('click', (e)=>{
@@ -23,41 +41,55 @@ const Header = () => {
     })
   },[])
   return (
-    <nav className="bg-white shadow-md z-50 sticky w-full left-0">
-      <Container>
-        <div className="lg:flex justify-between">
-          <div className="flex items-center">
-            <span className="text-xl font-bold text-blue-600">Final Inspection</span>
-          </div>
-          <div className="hidden lg:flex items-center">
-            <Link to="/" className="text-gray-700 hover:text-blue-600 lg:px-[15px] py-[20px]">Home</Link>
-            <Link to="/about" className="text-gray-700 hover:text-blue-600 lg:px-[15px] py-[20px]">About</Link>
-            <Link to="/about" className="text-gray-700 hover:text-blue-600 lg:px-[15px] py-[20px]">Contact</Link>
-
-            <div className="relative" ref={updateref}>
-              <button onClick={() => setIsUpdateOpen(!isUpdateOpen)} className="text-gray-700 hover:text-blue-600 focus:outline-none py-[20px] lg:px-[15px]">Update ▾</button>
-              {isUpdateOpen && (
-                <div className="absolute z-10 bg-white border border-gray-200 rounded shadow-md top-[100%] right-0 w-40">
-                  <Link to='/handover' href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-100">Handover</Link>
-                  <Link to='/shadehold' href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-100">Shade Hold</Link>
-                  <Link to='/excessfabric' href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-100">Excess Fabric</Link>
-                </div>
-              )}
+    <>
+      <nav className="bg-white shadow-md z-50 sticky w-full left-0">
+        <Container>
+          <div className="lg:flex justify-between items-center">
+            <div className="flex items-center">
+              <span className="text-xl font-bold text-blue-600">Final Inspection</span>
             </div>
-            <div className="relative" ref={reportref}>
-              <button onClick={() => setIsReportOpen(!isReportOpen)} className="text-gray-700 hover:text-blue-600 focus:outline-none py-[20px] lg:px-[15px]">Report ▾</button>
-              {isReportOpen && (
-                <div className="absolute z-10 bg-white border border-gray-200 rounded shadow-md top-[100%] right-0 w-40">
-                  <Link to='/handovershow' href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-100">Handover</Link>
-                  <Link to='/shadeholdshow' href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-100">Shade Hold</Link>
-                  <Link to='/excessfabricshow' href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-100">Excess Fabric</Link>
-                </div>
-              )}
+            <div className="hidden lg:flex items-center">
+              <Link to="/" className="text-gray-700 hover:text-blue-600 lg:px-[15px] py-[20px]">Home</Link>
+              <Link to="/about" className="text-gray-700 hover:text-blue-600 lg:px-[15px] py-[20px]">About</Link>
+              <Link to="/about" className="text-gray-700 hover:text-blue-600 lg:px-[15px] py-[20px]">Contact</Link>
+              {showupdate && 
+              <div className="relative" ref={updateref}>
+                <button onClick={() => setIsUpdateOpen(!isUpdateOpen)} className="text-gray-700 hover:text-blue-600 focus:outline-none py-[20px] lg:px-[15px]">Update ▾</button>
+                {isUpdateOpen && (
+                  <div className="absolute z-10 bg-white border border-gray-200 rounded shadow-md top-[100%] right-0 w-40">
+                    <Link to='/handover' href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-100">Handover</Link>
+                    <Link to='/shadehold' href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-100">Shade Hold</Link>
+                    <Link to='/excessfabric' href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-100">Excess Fabric</Link>
+                  </div>
+                )}
+              </div>
+              }
+              <div className="relative" ref={reportref}>
+                <button onClick={() => setIsReportOpen(!isReportOpen)} className="text-gray-700 hover:text-blue-600 focus:outline-none py-[20px] lg:px-[15px]">Report ▾</button>
+                {isReportOpen && (
+                  <div className="absolute z-10 bg-white border border-gray-200 rounded shadow-md top-[100%] right-0 w-40">
+                    <Link to='/handovershow' href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-100">Handover</Link>
+                    <Link to='/shadeholdshow' href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-100">Shade Hold</Link>
+                    <Link to='/excessfabricshow' href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-100">Excess Fabric</Link>
+                  </div>
+                )}
+              </div>
+            </div>
+            <div className="user">
+              <FaUser onClick={showUser}/>
             </div>
           </div>
+        </Container>
+      </nav>
+      {
+        showuser &&
+        <div className="absolute w-[200px] h-[250px] bg-[#ddd] left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]">
+          <input type="text" placeholder='User ID' onChange={(e)=>setUser(e.target.value)}/>
+          <input type="password" placeholder='Password' onChange={(e)=>setPassword(e.target.value)}/>
+          <button onClick={handleLogin}>Login</button>
         </div>
-      </Container>
-    </nav>
+      }
+    </>
   )
 }
 
